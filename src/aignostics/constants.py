de4aec4a1bf253be61427830e3e81dca44910ec2
Andreas Kunft <andreas@aignostics.com>
2025-04-13 18:16:31 +0200

    chore: Change default callback port to 8989

--- src/aignostics/constants.py
+++ src/aignostics/constants.py
@@ -12,7 +12,7 @@ API_ROOT_PRODUCTION = "https://platform.aignostics.com"
 AUDIENCE_PRODUCTION = "https://aignostics-platform-samia"
 AUTHORIZATION_BASE_URL_PRODUCTION = "https://aignostics-platform.eu.auth0.com/authorize"
 TOKEN_URL_PRODUCTION = "https://aignostics-platform.eu.auth0.com/oauth/token"  # noqa: S105
-REDIRECT_URI_PRODUCTION = "http://localhost:8080/"
+REDIRECT_URI_PRODUCTION = "http://localhost:8989/"
 DEVICE_URL_PRODUCTION = "https://aignostics-platform.eu.auth0.com/oauth/device/code"
 JWS_JSON_URL_PRODUCTION = "https://aignostics-platform.eu.auth0.com/.well-known/jwks.json"
 
@@ -29,6 +29,6 @@ API_ROOT_DEV = "https://platform-dev.aignostics.com"
 AUDIENCE_DEV = "https://dev-8ouohmmrbuh2h4vu-samia"
 AUTHORIZATION_BASE_URL_DEV = "https://dev-8ouohmmrbuh2h4vu.eu.auth0.com/authorize"
 TOKEN_URL_DEV = "https://dev-8ouohmmrbuh2h4vu.eu.auth0.com/oauth/token"  # noqa: S105
-REDIRECT_URI_DEV = "http://localhost:8080/"
+REDIRECT_URI_DEV = "http://localhost:8989/"
 DEVICE_URL_DEV = "https://dev-8ouohmmrbuh2h4vu.eu.auth0.com/oauth/device/code"
 JWS_JSON_URL_DEV = "https://dev-8ouohmmrbuh2h4vu.eu.auth0.com/.well-known/jwks.json"
