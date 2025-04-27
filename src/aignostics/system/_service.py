03440dd1569b9874deb1ab5fbe1963ff1caf86bd
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-04-27 18:03:54 +0200

    chore(deps): update from template

--- src/aignostics/system/_service.py
+++ src/aignostics/system/_service.py
@@ -36,6 +36,10 @@ log = get_logger(__name__)
 JsonType: t.TypeAlias = list["JsonValue"] | t.Mapping[str, "JsonValue"]
 JsonValue: t.TypeAlias = str | int | float | JsonType | None
 
+# Note: There is multiple measurements and network calls
+MEASURE_INTERVAL_SECONDS = 5
+NETWORK_TIMEOUT = 5
+
 
 class RuntimeDict(TypedDict, total=False):
     """Type for runtime information dictionary."""
@@ -108,7 +112,7 @@ class Service(BaseService):
         return token == self._settings.token.get_secret_value()
 
     @staticmethod
-    def _get_public_ipv4(timeout: int = 5) -> str | None:
+    def _get_public_ipv4(timeout: int = NETWORK_TIMEOUT) -> str | None:
         """Get the public IPv4 address of the system.
 
         Args:
@@ -162,8 +166,8 @@ class Service(BaseService):
         bootdatetime = boottime()
         vmem = psutil.virtual_memory()
         swap = psutil.swap_memory()
-        cpu_percent = psutil.cpu_percent(interval=2)
-        cpu_times_percent = psutil.cpu_times_percent(interval=2)
+        cpu_percent = psutil.cpu_percent(interval=MEASURE_INTERVAL_SECONDS)
+        cpu_times_percent = psutil.cpu_times_percent(interval=MEASURE_INTERVAL_SECONDS)
 
         rtn: InfoDict = {
             "package": {
