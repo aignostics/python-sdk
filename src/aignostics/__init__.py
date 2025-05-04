5bbcd0e9292676dcfeca7abcd48c2258e04ef4ad
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-05-04 09:50:24 +0200

    chore(windows): install and workaround auth/ssl issue

--- src/aignostics/__init__.py
+++ src/aignostics/__init__.py
@@ -3,18 +3,17 @@
 # TODO (Andreas): Fix CERTIFICATE_VERIFY_FAILED : unable to get local issuer certificate
 # HACK START to workaround
 import os
-import ssl
 
-import pip_system_certs.wrapt_requests  # noqa: F401
-
+# import ssl
+# import pip_system_certs.wrapt_requests
 from .constants import MODULES_TO_INSTRUMENT
 from .utils.boot import boot
 
-myssl = ssl.create_default_context()
-myssl.check_hostname = False
-myssl.verify_mode = ssl.CERT_NONE
-ssl._create_default_https_context = ssl._create_unverified_context
-os.environ["REQUESTS_CA_BUNDLE"] = ""
+# myssl = ssl.create_default_context()
+# myssl.check_hostname = False
+# myssl.verify_mode = ssl.CERT_NONE
+# ssl._create_default_https_context = ssl._create_unverified_context
+# os.environ["REQUESTS_CA_BUNDLE"] = ""
 
 # HACK END
 
