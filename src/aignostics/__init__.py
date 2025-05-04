e71164421f48507b421e4a39bd25b5f4df25bd34
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-05-04 08:36:44 +0200

    chore(windows): install and workaround auth/ssl issue

--- src/aignostics/__init__.py
+++ src/aignostics/__init__.py
@@ -1,6 +1,16 @@
 """Python SDK providing access to Aignostics AI services."""
 
+# TODO (Andreas): Fix CERTIFICATE_VERIFY_FAILED : unable to get local issuer certificate
+# HACK START to workaround
+import ssl
+
 from .constants import MODULES_TO_INSTRUMENT
 from .utils.boot import boot
+
+myssl = ssl.create_default_context()
+myssl.check_hostname = False
+myssl.verify_mode = ssl.CERT_NONE
+ssl._create_default_https_context = ssl._create_unverified_context
+# HACK END
 
 boot(modules_to_instrument=MODULES_TO_INSTRUMENT)
