99f90f8293e8de03ee1f56fd021f7671bc3ffee6
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-05-15 20:28:13 +0200

    chore: tests

--- tests/aignostics/docker_test.py
+++ tests/aignostics/docker_test.py
@@ -8,7 +8,7 @@ BUILT_WITH_LOVE = "built with love in Berlin"
 @pytest.mark.xdist_group(name="docker")
 @pytest.mark.skip_with_act
 @pytest.mark.docker
-# @pytest.mark.long_running
+@pytest.mark.long_running
 @pytest.mark.scheduled
 def test_core_docker_cli_help_with_love(docker_services) -> None:
     """Test the CLI help command with docker services returns expected output."""
