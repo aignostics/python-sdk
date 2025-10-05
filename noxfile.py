92adcf7d7a85815111cbccc0ce57b95bbad43a1a
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-10-05 23:50:13 +0200

    sec(gui): Introduce html-sanitizer, sanitizer footer. Rest is fine.

--- noxfile.py
+++ noxfile.py
@@ -92,8 +92,6 @@ def audit(session: nox.Session) -> None:
             "reports/vulnerabilities.json",
             "--ignore-vuln",
             "GHSA-4xh5-x5gv-qwph",
-            "--ignore-vuln",
-            "GHSA-8c95-hpq2-w46f",
         )  # https://pyinstaller.org/en/stable/license.html
     except CommandFailed:
         _format_json_with_jq(session, "reports/vulnerabilities.json")
