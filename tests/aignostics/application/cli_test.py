cdb67fa00a78cd55530a26421c721a9c49015581
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-12-09 14:35:41 +0100

    test: fix skip

--- tests/aignostics/application/cli_test.py
+++ tests/aignostics/application/cli_test.py
@@ -112,7 +112,7 @@ def test_cli_application_dump_schemata(runner: CliRunner, tmp_path: Path, record
 
 @pytest.mark.e2e
 @pytest.mark.timeout(timeout=60)
-@pytest.skip(reason="Test needs to be updated to changed error message")
+@pytest.mark.skip(reason="Test needs to be updated to changed error message")
 def test_cli_application_run_prepare_upload_submit_fail_on_mpp(
     runner: CliRunner, tmp_path: Path, record_property
 ) -> None:
