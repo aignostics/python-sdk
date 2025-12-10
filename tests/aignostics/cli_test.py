8be1f38cae7acf89f2410e4513bf7033c0ef474a
Helmut Hoffer von Ankershoffen <helmuthva@googlemail.com>
2025-12-10 07:23:45 +0100

    doc: add warnings for non-obvious behavior of uv(x)

--- tests/aignostics/cli_test.py
+++ tests/aignostics/cli_test.py
@@ -213,7 +213,7 @@ if find_spec("nicegui"):
                 "show_welcome_message parameter should be True when native is False"
             )
             assert mock_ui_run_args["show"] is True, "show parameter should be True when native is False"
-        if platform.system() == "Windows" and platform.python_version_tuple() >= ("3", "14"):
+        elif platform.system() == "Windows" and platform.python_version_tuple() >= ("3", "14"):
             assert mock_ui_run_args["native"] is False, "native parameter should be False on Windows"
             assert mock_ui_run_args["show_welcome_message"] is True, (
                 "show_welcome_message parameter should be True when native is False"
